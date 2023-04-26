ccbe8770fb3a37b3872d7555ae83ddb5bc3776cf
Ryan Pfeifle <thatastroguy@users.noreply.github.com>
2023-04-25 23:28:01 -0400

    Uncommented the previously commented commands. Copied some minor notes back into the NuSTAR version. Need to veryify that the CRVAL1 and 2 are correct for HEX-P v7.

--- sixte_duals_simulations.sh
+++ sixte_duals_simulations.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # SIXTE HEX-P Simulations for Dual AGNs and Mergers
-# R. W. Pfeifle
-# Creation Date: 29 December 2022
+# Author: R. W. Pfeifle
+# Creation Date: 29 December 2023
 # Last Revision: 25 April 2023
 
 ARG1="$1" # Choice of separation
@@ -19,34 +19,33 @@ export SIXTE=/Users/ryan/simput
 export HEADASNOQUERY=
 export HEADASPROMPT=/dev/null
 
-
 #After generating the simput files, we now need to merge the simput files together before simulating the observation
 
 #xmldir=${SIXTE}share/instruments
 #xml=${xmldir}/hex-p/het/hexp_het_ff.xml
 
 # Here we're generating the HET1 (1 of 2) event file using our simput file from SOXS...
-##$SIXTE/bin/runsixt XMLFile=${SIXTE}/share/instruments/hex-p/het/hexp_het_ff.xml Prefix=sixtesim_ RA=${ARG2} Dec=${ARG3} Simput=dualagn_${ARG1}as_simput${ARG5}.fits EvtFile=${ARG1}as_evt${ARG4%???}ks_HET1${ARG5}.fits Exposure=${ARG4}
-### Here we're generating the HET2 (2 of 2) event file using our simput file from SOXS...
-##$SIXTE/bin/runsixt XMLFile=${SIXTE}/share/instruments/hex-p/het/hexp_het_ff.xml Prefix=sixtesim_ RA=${ARG2} Dec=${ARG3} Simput=dualagn_${ARG1}as_simput${ARG5}.fits EvtFile=${ARG1}as_evt${ARG4%???}ks_HET2${ARG5}.fits Exposure=${ARG4}
-##
-### Here we're generating the accompanying LET image, using the simput file from SOXS....
-##$SIXTE/bin/runsixt XMLFile=${SIXTE}/share/instruments/hex-p/let/hexp_let_LEO_ff.xml Prefix=sixtesim_ RA=${ARG2} Dec=${ARG3} Simput=dualagn_${ARG1}as_simput${ARG5}.fits EvtFile=${ARG1}as_evt${ARG4%???}ks_LET${ARG5}.fits Exposure=${ARG4}
-##
-### Here we're using the radec2xy tool to add X,Y sky coordinates and a WCS to the event files...
-##$SIXTE/bin/radec2xy EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_HET1${ARG5}.fits projection=TAN RefRA=${ARG2} RefDec=${ARG3}
-##$SIXTE/bin/radec2xy EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_HET2${ARG5}.fits projection=TAN RefRA=${ARG2} RefDec=${ARG3}
-##$SIXTE/bin/radec2xy EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_LET${ARG5}.fits projection=TAN RefRA=${ARG2} RefDec=${ARG3}
-##
-### Here we are generating a combined HET image, saved as 2HETeff
-##ftmerge \
-##sixtesim_${ARG1}as_evt${ARG4%???}ks_HET1${ARG5}.fits,sixtesim_${ARG1}as_evt${ARG4%???}ks_HET2${ARG5}.fits \
-##sixtesim_${ARG1}as_evt${ARG4%???}ks_2HETeff${ARG5}.fits clobber=yes
-##
-### Here we're generating image files for each HET (just in case we want to compare them to the combined image) and the LET
-##$SIXTE/bin/imgev EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_HET1${ARG5}.fits Image=IMAGE_${ARG1}as_img${ARG4%???}ks_HET1${ARG5}.fits CoordinateSystem=0 Projection=TAN NAXIS1=640 NAXIS2=640 CUNIT1=deg CUNIT2=deg CRVAL1=${ARG2} CRVAL2=${ARG3} CRPIX1=320.5 CRPIX2=320.5 CDELT1=-3.460665e-4 CDELT2=3.460665e-4 history=true clobber=yes
-##$SIXTE/bin/imgev EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_HET2${ARG5}.fits Image=IMAGE_${ARG1}as_img${ARG4%???}ks_HET2${ARG5}.fits CoordinateSystem=0 Projection=TAN NAXIS1=640 NAXIS2=640 CUNIT1=deg CUNIT2=deg CRVAL1=${ARG2} CRVAL2=${ARG3} CRPIX1=320.5 CRPIX2=320.5 CDELT1=-3.460665e-4 CDELT2=3.460665e-4 history=true clobber=yes
-##$SIXTE/bin/imgev EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_LET${ARG5}.fits Image=IMAGE_${ARG1}as_img${ARG4%???}ks_LET${ARG5}.fits CoordinateSystem=0 Projection=TAN NAXIS1=512 NAXIS2=512 CUNIT1=deg CUNIT2=deg CRVAL1=${ARG2} CRVAL2=${ARG3} CRPIX1=256.5 CRPIX2=256.5 CDELT1=-3.7242256e-4 CDELT2=3.7242256e-4 history=true clobber=yes
+$SIXTE/bin/runsixt XMLFile=${SIXTE}/share/instruments/hex-p/het/hexp_het_ff.xml Prefix=sixtesim_ RA=${ARG2} Dec=${ARG3} Simput=dualagn_${ARG1}as_simput${ARG5}.fits EvtFile=${ARG1}as_evt${ARG4%???}ks_HET1${ARG5}.fits Exposure=${ARG4}
+# Here we're generating the HET2 (2 of 2) event file using our simput file from SOXS...
+$SIXTE/bin/runsixt XMLFile=${SIXTE}/share/instruments/hex-p/het/hexp_het_ff.xml Prefix=sixtesim_ RA=${ARG2} Dec=${ARG3} Simput=dualagn_${ARG1}as_simput${ARG5}.fits EvtFile=${ARG1}as_evt${ARG4%???}ks_HET2${ARG5}.fits Exposure=${ARG4}
+
+# Here we're generating the accompanying LET image, using the simput file from SOXS....
+$SIXTE/bin/runsixt XMLFile=${SIXTE}/share/instruments/hex-p/let/hexp_let_LEO_ff.xml Prefix=sixtesim_ RA=${ARG2} Dec=${ARG3} Simput=dualagn_${ARG1}as_simput${ARG5}.fits EvtFile=${ARG1}as_evt${ARG4%???}ks_LET${ARG5}.fits Exposure=${ARG4}
+
+# Here we're using the radec2xy tool to add X,Y sky coordinates and a WCS to the event files...
+$SIXTE/bin/radec2xy EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_HET1${ARG5}.fits projection=TAN RefRA=${ARG2} RefDec=${ARG3}
+$SIXTE/bin/radec2xy EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_HET2${ARG5}.fits projection=TAN RefRA=${ARG2} RefDec=${ARG3}
+$SIXTE/bin/radec2xy EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_LET${ARG5}.fits projection=TAN RefRA=${ARG2} RefDec=${ARG3}
+
+# Here we are generating a combined HET image, saved as 2HETeff
+ftmerge \
+sixtesim_${ARG1}as_evt${ARG4%???}ks_HET1${ARG5}.fits,sixtesim_${ARG1}as_evt${ARG4%???}ks_HET2${ARG5}.fits \
+sixtesim_${ARG1}as_evt${ARG4%???}ks_2HETeff${ARG5}.fits clobber=yes
+
+# Here we're generating image files for each HET (just in case we want to compare them to the combined image) and the LET
+$SIXTE/bin/imgev EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_HET1${ARG5}.fits Image=IMAGE_${ARG1}as_img${ARG4%???}ks_HET1${ARG5}.fits CoordinateSystem=0 Projection=TAN NAXIS1=640 NAXIS2=640 CUNIT1=deg CUNIT2=deg CRVAL1=${ARG2} CRVAL2=${ARG3} CRPIX1=320.5 CRPIX2=320.5 CDELT1=-3.460665e-4 CDELT2=3.460665e-4 history=true clobber=yes
+$SIXTE/bin/imgev EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_HET2${ARG5}.fits Image=IMAGE_${ARG1}as_img${ARG4%???}ks_HET2${ARG5}.fits CoordinateSystem=0 Projection=TAN NAXIS1=640 NAXIS2=640 CUNIT1=deg CUNIT2=deg CRVAL1=${ARG2} CRVAL2=${ARG3} CRPIX1=320.5 CRPIX2=320.5 CDELT1=-3.460665e-4 CDELT2=3.460665e-4 history=true clobber=yes
+$SIXTE/bin/imgev EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_LET${ARG5}.fits Image=IMAGE_${ARG1}as_img${ARG4%???}ks_LET${ARG5}.fits CoordinateSystem=0 Projection=TAN NAXIS1=512 NAXIS2=512 CUNIT1=deg CUNIT2=deg CRVAL1=${ARG2} CRVAL2=${ARG3} CRPIX1=256.5 CRPIX2=256.5 CDELT1=-3.7242256e-4 CDELT2=3.7242256e-4 history=true clobber=yes
 
 # Here we're generating the image files from the effective 2 camera HET image and the LET image
 $SIXTE/bin/imgev EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_2HETeff${ARG5}.fits Image=IMAGE_${ARG1}as_img${ARG4%???}ks_2HETeff${ARG5}.fits CoordinateSystem=0 Projection=TAN NAXIS1=640 NAXIS2=640 CUNIT1=deg CUNIT2=deg CRVAL1=${ARG2} CRVAL2=${ARG3} CRPIX1=320.5 CRPIX2=320.5 CDELT1=-3.460665e-4 CDELT2=3.460665e-4 history=true clobber=yes
@@ -60,57 +59,57 @@ $SIXTE/bin/imgev EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_2HETeff${ARG5}.fits
 # and we will always extract two spectra, even in the case of convolved sources (for simplicity of the code).
 
 # Extraction for AGN 1. Extracting from HET 1, HET 2, and the LET
-###$SIXTE/bin/makespec \
-###EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_HET1${ARG5}.fits \
-###Spectrum=SPEC_AGN1_${ARG1}as_${ARG4%???}ks_HET1${ARG5}.pha \
-###EventFilter="regfilter(\"cir1_${ARG1}.reg\")" \
-###RSPPath=${SIXTE}/share/instruments/hex-p/het/ clobber=yes #,RA,DEC
-###
-###$SIXTE/bin/makespec \
-###EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_HET2${ARG5}.fits \
-###Spectrum=SPEC_AGN1_${ARG1}as_${ARG4%???}ks_HET2${ARG5}.pha \
-###EventFilter="regfilter(\"cir1_${ARG1}.reg\")" \
-###RSPPath=${SIXTE}/share/instruments/hex-p/het/ clobber=yes
-###
-###$SIXTE/bin/makespec \
-###EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_LET${ARG5}.fits \
-###Spectrum=SPEC_AGN1_${ARG1}as_${ARG4%???}ks_LET${ARG5}.pha \
-###EventFilter="regfilter(\"cir1_${ARG1}.reg\")" \
-###RSPPath=${SIXTE}/share/instruments/hex-p/let/ clobber=yes
-###
-###
-#### Extraction for AGN 2. Extracting from HET 1, HET 2, and the LET
-###$SIXTE/bin/makespec \
-###EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_HET1${ARG5}.fits \
-###Spectrum=SPEC_AGN2_${ARG1}as_${ARG4%???}ks_HET1${ARG5}.pha \
-###EventFilter="regfilter(\"cir2_${ARG1}.reg\")" \
-###RSPPath=${SIXTE}/share/instruments/hex-p/het/ clobber=yes
-###
-###$SIXTE/bin/makespec \
-###EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_HET2${ARG5}.fits \
-###Spectrum=SPEC_AGN2_${ARG1}as_${ARG4%???}ks_HET2${ARG5}.pha \
-###EventFilter="regfilter(\"cir2_${ARG1}.reg\")" \
-###RSPPath=${SIXTE}/share/instruments/hex-p/het/ clobber=yes
-###
-###$SIXTE/bin/makespec \
-###EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_LET${ARG5}.fits \
-###Spectrum=SPEC_AGN2_${ARG1}as_${ARG4%???}ks_LET${ARG5}.pha \
-###EventFilter="regfilter(\"cir2_${ARG1}.reg\")" \
-###RSPPath=${SIXTE}/share/instruments/hex-p/let/ clobber=yes
+$SIXTE/bin/makespec \
+EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_HET1${ARG5}.fits \
+Spectrum=SPEC_AGN1_${ARG1}as_${ARG4%???}ks_HET1${ARG5}.pha \
+EventFilter="regfilter(\"cir1_${ARG1}.reg\")" \
+RSPPath=${SIXTE}/share/instruments/hex-p/het/ clobber=yes #,RA,DEC
+
+$SIXTE/bin/makespec \
+EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_HET2${ARG5}.fits \
+Spectrum=SPEC_AGN1_${ARG1}as_${ARG4%???}ks_HET2${ARG5}.pha \
+EventFilter="regfilter(\"cir1_${ARG1}.reg\")" \
+RSPPath=${SIXTE}/share/instruments/hex-p/het/ clobber=yes
+
+$SIXTE/bin/makespec \
+EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_LET${ARG5}.fits \
+Spectrum=SPEC_AGN1_${ARG1}as_${ARG4%???}ks_LET${ARG5}.pha \
+EventFilter="regfilter(\"cir1_${ARG1}.reg\")" \
+RSPPath=${SIXTE}/share/instruments/hex-p/let/ clobber=yes
+
+
+# Extraction for AGN 2. Extracting from HET 1, HET 2, and the LET
+$SIXTE/bin/makespec \
+EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_HET1${ARG5}.fits \
+Spectrum=SPEC_AGN2_${ARG1}as_${ARG4%???}ks_HET1${ARG5}.pha \
+EventFilter="regfilter(\"cir2_${ARG1}.reg\")" \
+RSPPath=${SIXTE}/share/instruments/hex-p/het/ clobber=yes
+
+$SIXTE/bin/makespec \
+EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_HET2${ARG5}.fits \
+Spectrum=SPEC_AGN2_${ARG1}as_${ARG4%???}ks_HET2${ARG5}.pha \
+EventFilter="regfilter(\"cir2_${ARG1}.reg\")" \
+RSPPath=${SIXTE}/share/instruments/hex-p/het/ clobber=yes
+
+$SIXTE/bin/makespec \
+EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_LET${ARG5}.fits \
+Spectrum=SPEC_AGN2_${ARG1}as_${ARG4%???}ks_LET${ARG5}.pha \
+EventFilter="regfilter(\"cir2_${ARG1}.reg\")" \
+RSPPath=${SIXTE}/share/instruments/hex-p/let/ clobber=yes
 
 
 # Now for the background spectra
-##$SIXTE/bin/makespec \
-##EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_HET1${ARG5}.fits \
-##Spectrum=SPEC_BKG_${ARG1}as_${ARG4%???}ks_HET1${ARG5}.pha \
-##EventFilter="regfilter(\"bkg_${ARG1}.reg\")" \
-##RSPPath=${SIXTE}/share/instruments/hex-p/het/ clobber=yes #,RA,DEC
-##
-##$SIXTE/bin/makespec \
-##EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_HET2${ARG5}.fits \
-##Spectrum=SPEC_BKG_${ARG1}as_${ARG4%???}ks_HET2${ARG5}.pha \
-##EventFilter="regfilter(\"bkg_${ARG1}.reg\")" \
-##RSPPath=${SIXTE}/share/instruments/hex-p/het/ clobber=yes
+$SIXTE/bin/makespec \
+EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_HET1${ARG5}.fits \
+Spectrum=SPEC_BKG_${ARG1}as_${ARG4%???}ks_HET1${ARG5}.pha \
+EventFilter="regfilter(\"bkg_${ARG1}.reg\")" \
+RSPPath=${SIXTE}/share/instruments/hex-p/het/ clobber=yes #,RA,DEC
+
+$SIXTE/bin/makespec \
+EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_HET2${ARG5}.fits \
+Spectrum=SPEC_BKG_${ARG1}as_${ARG4%???}ks_HET2${ARG5}.pha \
+EventFilter="regfilter(\"bkg_${ARG1}.reg\")" \
+RSPPath=${SIXTE}/share/instruments/hex-p/het/ clobber=yes
 
 $SIXTE/bin/makespec \
 EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_LET${ARG5}.fits \
--- sixte_nustar_duals_simulations.sh
+++ sixte_nustar_duals_simulations.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # SIXTE NuSTAR Simulations for Dual AGNs and Mergers
-# R. W. Pfeifle
-# Creation Date: 29 December 2022
+# Author: R. W. Pfeifle
+# Creation Date: 25 April 2023 (branched from sixte_duals_simulations.sh)
 # Last Revision: 25 April 2023
 
 ARG1="$1" # Choice of separation
@@ -24,49 +24,57 @@ export HEADASPROMPT=/dev/null
 #xmldir=${SIXTE}share/instruments
 #xml=${xmldir}/hex-p/het/hexp_het_ff.xml
 
-#### Here we're generating the FPMA (1 of 2) event file using our simput file from SOXS...
-###$SIXTE/bin/runsixt XMLFile=${SIXTE}/share/instruments/nustar/nustar.xml Prefix=sixtesim_ RA=${ARG2} Dec=${ARG3} Simput=dualagn_${ARG1}as_simput${ARG5}.fits EvtFile=${ARG1}as_evt${ARG4%???}ks_FPMA${ARG5}.fits Exposure=${ARG4}
-#### Here we're generating the FPMB (2 of 2) event file using our simput file from SOXS...
-###$SIXTE/bin/runsixt XMLFile=${SIXTE}/share/instruments/nustar/nustar.xml Prefix=sixtesim_ RA=${ARG2} Dec=${ARG3} Simput=dualagn_${ARG1}as_simput${ARG5}.fits EvtFile=${ARG1}as_evt${ARG4%???}ks_FPMB${ARG5}.fits Exposure=${ARG4}
-###
-#### Here we're using the radec2xy tool to add X,Y sky coordinates and a WCS to the event files...
-###$SIXTE/bin/radec2xy EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_FPMA${ARG5}.fits projection=TAN RefRA=${ARG2} RefDec=${ARG3}
-###$SIXTE/bin/radec2xy EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_FPMB${ARG5}.fits projection=TAN RefRA=${ARG2} RefDec=${ARG3}
-###
-#### Here we're generating image files for each HET (just in case we want to compare them to the combined image) and the LET
-###$SIXTE/bin/imgev EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_FPMA${ARG5}.fits Image=IMAGE_${ARG1}as_img${ARG4%???}ks_FPMA${ARG5}.fits CoordinateSystem=0 Projection=TAN NAXIS1=325 NAXIS2=325 CUNIT1=deg CUNIT2=deg CRVAL1=${ARG2} CRVAL2=${ARG3} CRPIX1=190.5 CRPIX2=135.5 CDELT1=-6.7805656e-4 CDELT2=6.7805656e-4 history=true clobber=yes
-###$SIXTE/bin/imgev EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_FPMB${ARG5}.fits Image=IMAGE_${ARG1}as_img${ARG4%???}ks_FPMB${ARG5}.fits CoordinateSystem=0 Projection=TAN NAXIS1=325 NAXIS2=325 CUNIT1=deg CUNIT2=deg CRVAL1=${ARG2} CRVAL2=${ARG3} CRPIX1=190.5 CRPIX2=135.5 CDELT1=-6.7805656e-4 CDELT2=6.7805656e-4 history=true clobber=yes
-###
-#### Here we are generating a combined HET image, saved as 2HETeff
-###ftmerge \
-###sixtesim_${ARG1}as_evt${ARG4%???}ks_FPMA${ARG5}.fits,sixtesim_${ARG1}as_evt${ARG4%???}ks_FPMB${ARG5}.fits \
-###sixtesim_${ARG1}as_evt${ARG4%???}ks_2FPMeff${ARG5}.fits clobber=yes
-###
-#### Here we're generating the image files from the effective 2 camera FPM image
-###$SIXTE/bin/imgev EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_2FPMeff${ARG5}.fits Image=IMAGE_${ARG1}as_img${ARG4%???}ks_2FPMeff${ARG5}.fits CoordinateSystem=0 Projection=TAN NAXIS1=325 NAXIS2=325 CUNIT1=deg CUNIT2=deg CRVAL1=${ARG2} CRVAL2=${ARG3} CRPIX1=190.5 CRPIX2=135.5 CDELT1=-6.7805656e-4 CDELT2=6.7805656e-4 history=true clobber=yes
-###
-#### Extraction for AGN 1. Extracting from FPMA and FPMB
-###$SIXTE/bin/makespec \
-###EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_FPMA.fits \
-###Spectrum=SPEC_AGN1_${ARG1}as_${ARG4%???}ks_FPMA.pha \
-###EventFilter="regfilter(\"cir1_${ARG1}.reg\")" \
-###RSPPath=${SIXTE}/share/instruments/nustar/ clobber=yes #,RA,DEC
-###
-###$SIXTE/bin/makespec \
-###EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_FPMB.fits \
-###Spectrum=SPEC_AGN1_${ARG1}as_${ARG4%???}ks_FPMB.pha \
-###EventFilter="regfilter(\"cir1_${ARG1}.reg\")" \
-###RSPPath=${SIXTE}/share/instruments/nustar/ clobber=yes
-###
-#### Extraction for AGN 2. Extracting from FPMA and FPMB
-###$SIXTE/bin/makespec \
-###EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_FPMA.fits \
-###Spectrum=SPEC_AGN2_${ARG1}as_${ARG4%???}ks_FPMA.pha \
-###EventFilter="regfilter(\"cir2_${ARG1}.reg\")" \
-###RSPPath=${SIXTE}/share/instruments/nustar/ clobber=yes
-###
-###$SIXTE/bin/makespec \
-###EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_FPMB.fits \
-###Spectrum=SPEC_AGN2_${ARG1}as_${ARG4%???}ks_FPMB.pha \
-###EventFilter="regfilter(\"cir2_${ARG1}.reg\")" \
-###RSPPath=${SIXTE}/share/instruments/nustar/ clobber=yes
+# Here we're generating the FPMA (1 of 2) event file using our simput file from SOXS...
+$SIXTE/bin/runsixt XMLFile=${SIXTE}/share/instruments/nustar/nustar.xml Prefix=sixtesim_ RA=${ARG2} Dec=${ARG3} Simput=dualagn_${ARG1}as_simput${ARG5}.fits EvtFile=${ARG1}as_evt${ARG4%???}ks_FPMA${ARG5}.fits Exposure=${ARG4}
+# Here we're generating the FPMB (2 of 2) event file using our simput file from SOXS...
+$SIXTE/bin/runsixt XMLFile=${SIXTE}/share/instruments/nustar/nustar.xml Prefix=sixtesim_ RA=${ARG2} Dec=${ARG3} Simput=dualagn_${ARG1}as_simput${ARG5}.fits EvtFile=${ARG1}as_evt${ARG4%???}ks_FPMB${ARG5}.fits Exposure=${ARG4}
+
+# Here we're using the radec2xy tool to add X,Y sky coordinates and a WCS to the event files...
+$SIXTE/bin/radec2xy EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_FPMA${ARG5}.fits projection=TAN RefRA=${ARG2} RefDec=${ARG3}
+$SIXTE/bin/radec2xy EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_FPMB${ARG5}.fits projection=TAN RefRA=${ARG2} RefDec=${ARG3}
+
+# Here we're generating image files for each HET (just in case we want to compare them to the combined image) and the LET
+$SIXTE/bin/imgev EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_FPMA${ARG5}.fits Image=IMAGE_${ARG1}as_img${ARG4%???}ks_FPMA${ARG5}.fits CoordinateSystem=0 Projection=TAN NAXIS1=325 NAXIS2=325 CUNIT1=deg CUNIT2=deg CRVAL1=${ARG2} CRVAL2=${ARG3} CRPIX1=190.5 CRPIX2=135.5 CDELT1=-6.7805656e-4 CDELT2=6.7805656e-4 history=true clobber=yes
+$SIXTE/bin/imgev EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_FPMB${ARG5}.fits Image=IMAGE_${ARG1}as_img${ARG4%???}ks_FPMB${ARG5}.fits CoordinateSystem=0 Projection=TAN NAXIS1=325 NAXIS2=325 CUNIT1=deg CUNIT2=deg CRVAL1=${ARG2} CRVAL2=${ARG3} CRPIX1=190.5 CRPIX2=135.5 CDELT1=-6.7805656e-4 CDELT2=6.7805656e-4 history=true clobber=yes
+
+# Here we are generating a combined HET image, saved as 2HETeff
+ftmerge \
+sixtesim_${ARG1}as_evt${ARG4%???}ks_FPMA${ARG5}.fits,sixtesim_${ARG1}as_evt${ARG4%???}ks_FPMB${ARG5}.fits \
+sixtesim_${ARG1}as_evt${ARG4%???}ks_2FPMeff${ARG5}.fits clobber=yes
+
+# Here we're generating the image files from the effective 2 camera FPM image
+$SIXTE/bin/imgev EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_2FPMeff${ARG5}.fits Image=IMAGE_${ARG1}as_img${ARG4%???}ks_2FPMeff${ARG5}.fits CoordinateSystem=0 Projection=TAN NAXIS1=325 NAXIS2=325 CUNIT1=deg CUNIT2=deg CRVAL1=${ARG2} CRVAL2=${ARG3} CRPIX1=190.5 CRPIX2=135.5 CDELT1=-6.7805656e-4 CDELT2=6.7805656e-4 history=true clobber=yes
+# Note: 
+#   CRVAL1: RA coordinate for observation
+#   CRVAL2: Dec coordinate for observation
+# The above values MUST match the original RA and Dec values of the simulated event file!!
+# Calculate CDELT by taking xdelt and ydelt in the xml file (in units of m) and dividing by 20m (the focal length) and multiplying by 180/PI
+
+# Now we'll extract spectra for the two positions of the AGNs. In these simulations, we always assume there are two AGNs, \
+# and we will always extract two spectra, even in the case of convolved sources (for simplicity of the code).
+
+# Extraction for AGN 1. Extracting from FPMA and FPMB
+$SIXTE/bin/makespec \
+EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_FPMA.fits \
+Spectrum=SPEC_AGN1_${ARG1}as_${ARG4%???}ks_FPMA.pha \
+EventFilter="regfilter(\"cir1_${ARG1}.reg\")" \
+RSPPath=${SIXTE}/share/instruments/nustar/ clobber=yes #,RA,DEC
+
+$SIXTE/bin/makespec \
+EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_FPMB.fits \
+Spectrum=SPEC_AGN1_${ARG1}as_${ARG4%???}ks_FPMB.pha \
+EventFilter="regfilter(\"cir1_${ARG1}.reg\")" \
+RSPPath=${SIXTE}/share/instruments/nustar/ clobber=yes
+
+# Extraction for AGN 2. Extracting from FPMA and FPMB
+$SIXTE/bin/makespec \
+EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_FPMA.fits \
+Spectrum=SPEC_AGN2_${ARG1}as_${ARG4%???}ks_FPMA.pha \
+EventFilter="regfilter(\"cir2_${ARG1}.reg\")" \
+RSPPath=${SIXTE}/share/instruments/nustar/ clobber=yes
+
+$SIXTE/bin/makespec \
+EvtFile=sixtesim_${ARG1}as_evt${ARG4%???}ks_FPMB.fits \
+Spectrum=SPEC_AGN2_${ARG1}as_${ARG4%???}ks_FPMB.pha \
+EventFilter="regfilter(\"cir2_${ARG1}.reg\")" \
+RSPPath=${SIXTE}/share/instruments/nustar/ clobber=yes
